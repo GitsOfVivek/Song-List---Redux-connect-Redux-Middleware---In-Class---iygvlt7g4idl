3d8367d9b2ac7202d583123aed3d467971e595ba
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-28 11:04:51 +0530

    Done

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,18 +1,27 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 function SongList(props) {
-    const { songs } = props;
-    return (
-        <div>
-            {songs.map((song) => (
-                <div className="song">{song.title}</div>
-            ))}
-        </div>
-    );
+	const { songs } = props;
+	return (
+		<div>
+			{songs.map(song => (
+				<div className="song">{song.title}</div>
+			))}
+		</div>
+	);
 }
 
-const mapStateToProps = (state) => {
-    return { songs: state.songs };
+const mapStateToProps = state => {
+	return { songs: state.songs };
 };
 
 // Add code here
+
+import { selectSong } from '../actions';
+const mapDispatchToProps = dispatch => {
+	return {
+		selectSong: song => dispatch(selectSong(song)),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SongList);
